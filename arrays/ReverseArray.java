a3f122ee16170a9025ed5f676578083cb977a044
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-02-25 20:37:35 +0530

    minor fix: package error fix

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -1,4 +1,4 @@
-package arrays;
+
 
 final public class ReverseArray {
     public static void main(String[] args) {
--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -1,4 +1,3 @@
-package arrays;
 
 final class SecondLargest {
     public static void main(String[] args) {

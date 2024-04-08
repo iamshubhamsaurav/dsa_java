c9d467f39630f984c9d848780c914e1175574550
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-04-09 00:07:44 +0530

    hashmap basic file added

---
+++ basics/hash_maps/BasicsHashMap.java
@@ -0,0 +1,15 @@
+package basics.hash_maps;
+
+import java.util.*;;
+
+public class BasicsHashMap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(0, "Shubham");
+        map.put(1, "Saurav");
+        map.put(2, "ABC");
+        map.put(3, "DEF");
+
+        System.out.println(map);
+    }
+}

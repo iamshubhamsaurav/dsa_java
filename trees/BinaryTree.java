93ea7235138e65c14dcd676c97ba565a17713659
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-04-14 23:00:13 +0530

    Binary Tree Traversal

--- trees/BinaryTree.java
+++ trees/BinaryTree.java
@@ -5,6 +5,32 @@ public class BinaryTree {
         Node newNode = new Node(1);
         newNode.left = new Node(2);
         newNode.right = new Node(3);
+
+        inOrder(newNode);
+    }
+
+    static void inOrder(Node root) {
+        if(root != null) {
+            inOrder(root.left);
+            System.out.println(root.key);
+            inOrder(root.right);
+        }
+    }
+
+    static void preOrder(Node root) {
+        if(root != null) {
+            System.out.println(root.key);
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    static void postOrder(Node root) {
+        if(root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.println(root.key);
+        }
     }
 }
 

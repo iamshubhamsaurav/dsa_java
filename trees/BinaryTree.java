841461dc79e74d3b18f95fd71f8c646b358ef0ab
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-04-16 20:13:32 +0530

    BFS or Level Order Traversal method added

--- trees/BinaryTree.java
+++ trees/BinaryTree.java
@@ -1,14 +1,24 @@
 package trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
     public static void main(String[] args) {
         Node newNode = new Node(1);
         newNode.left = new Node(2);
         newNode.right = new Node(3);
+        newNode.left.left = new Node(4);
+        newNode.left.right = new Node(5);
+
+        newNode.right.left = new Node(6);
+        newNode.right.right = new Node(7);
 
         inOrder(newNode);
         System.out.println("Height: " + height(newNode));
         printKDistantNode(newNode, 1);
+        System.out.println("Level Order Traversal Below");
+        levelOrderTraversal(newNode);
     }
 
     static void inOrder(Node root) {
@@ -49,6 +59,22 @@ public class BinaryTree {
             printKDistantNode(root.left, k - 1);
             printKDistantNode(root.right, k - 1);
         } 
+    }
+
+    static void levelOrderTraversal(Node root) {
+        if(root == null) return;
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while(q.isEmpty() == false) {
+            Node curr = q.poll();
+            System.out.println(curr.key);
+            if(curr.left != null) {
+                q.add(curr.left);
+            }
+            if(curr.right != null) {
+                q.add(curr.right);
+            }
+        }
     }
 
 }

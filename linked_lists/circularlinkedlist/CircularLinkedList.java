773120b458c6ee9a8e1253b5e7f640fe9242c497
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-04-19 01:24:15 +0530

    Circular List Traversal added

--- linked_lists/circularlinkedlist/CircularLinkedList.java
+++ linked_lists/circularlinkedlist/CircularLinkedList.java
@@ -2,7 +2,22 @@ package linked_lists.circularlinkedlist;
 
 public class CircularLinkedList {
     public static void main(String[] args) {
-        
+        Node node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        node.next.next.next = node;
+
+        traverse(node);
+    }
+
+    static void traverse(Node node) {
+        Node curr = node.next;
+        System.out.println(node.data + " ");
+
+        while (curr != node) {
+            System.out.println(curr.data);
+            curr = curr.next;       
+        }
     }
 }
 

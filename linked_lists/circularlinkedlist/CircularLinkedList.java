76b5d0af02a5d5cdc23457916d15b86c5c913c86
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-04-25 20:46:57 +0530

    insertAtBegin efficient solution added

--- linked_lists/circularlinkedlist/CircularLinkedList.java
+++ linked_lists/circularlinkedlist/CircularLinkedList.java
@@ -6,8 +6,9 @@ public class CircularLinkedList {
         node.next = new Node(2);
         node.next.next = new Node(3);
         node.next.next.next = node;
-
+        node = insertAtBeginning(node, 0);
         traverse(node);
+        
     }
 
     static void traverse(Node node) {
@@ -19,6 +20,49 @@ public class CircularLinkedList {
             System.out.println(curr.data);
             curr = curr.next;       
         }
+    }
+
+    static Node insertAtBeginning(Node head, int value) {
+        Node newNode = new Node(value);
+        newNode.next = null;
+            
+        // handle error/ edge case
+        if(head == null) {
+            head = newNode;
+            return head;
+        }
+
+        // make the newNode the next node of head
+        newNode.next = head.next;
+        head.next = newNode;
+
+        // Swap the values of the head and newNode
+        int temp = newNode.data;
+        newNode.data = head.data;
+        head.data = temp;
+
+        return head;
+    }
+
+    // Niave Solution
+    static Node insertAtBeginningNiave(Node head, int value) {
+        Node newNode = new Node(value);
+        if(head == null) {
+            head = newNode;
+            head.next = head;
+        } else if (head.next == null) {
+            head.next = newNode;
+            newNode.next = head;
+        } else {
+            Node curr = head.next;
+            while(curr.next != head) {
+                curr = curr.next;
+            }
+            newNode.next = head;
+            curr.next = newNode;
+            head = newNode;
+        }
+        return head;
     }
 }
 

eeb3142d8fafd4e7b7777fe65e363e339894c5ae
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-04-26 21:19:10 +0530

    pre, in and post recursive traversal implemented

--- trees/binary_trees/BinaryTree2.java
+++ trees/binary_trees/BinaryTree2.java
@@ -28,6 +28,27 @@ public class BinaryTree2 {
         }
     }
 
+    static void preorder(Node root) {
+        if(root == null) return;
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    static void inorder(Node root) {
+        if(root == null) return;
+        preorder(root.left);
+        System.out.print(root.data + " ");
+        preorder(root.right);
+    }
+
+    static void postorder(Node root) {
+        if(root == null) return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
     public static void main(String[] args) {
 
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};

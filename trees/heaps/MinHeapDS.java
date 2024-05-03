56d3f7ba87cabb36a565f014f97a27a4664942c1
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-05-03 20:00:21 +0530

    left(i), right(i) and parent(i) method added

--- trees/heaps/MinHeapDS.java
+++ trees/heaps/MinHeapDS.java
@@ -10,4 +10,17 @@ public class MinHeapDS {
         arr = new int[capacity];
         this.size = 0;
     }
+
+    int left(int i) {
+        return (2 * i + 1);
+    }
+
+    int right(int i) {
+        return (2 * i + 2);
+    }
+
+    int parent(int i) {
+        return (i - 1) / 2; // floor value
+    }
+
 }

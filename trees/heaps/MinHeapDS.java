dc70f45d4b5fd8e3aae61b5dd3f80ef74073de26
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-05-03 21:09:45 +0530

    insert method added

--- trees/heaps/MinHeapDS.java
+++ trees/heaps/MinHeapDS.java
@@ -23,4 +23,21 @@ public class MinHeapDS {
         return (i - 1) / 2; // floor value
     }
 
+    void insert(int val) {
+        if (this.size == this.capacity) {
+            return;
+        }
+        size++;
+        arr[size - 1] = val;
+
+        for(int i = this.size - 1; i != 0 && arr[parent(i)] > arr[i];) {
+            int temp = arr[i];
+            arr[i] = arr[parent(i)];
+            arr[parent(i)] = temp;
+
+            // Update the i
+            i = parent(i);
+        }
+    }
+
 }

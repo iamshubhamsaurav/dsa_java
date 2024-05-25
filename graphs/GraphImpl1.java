5b588a59374c7c36c59ec8e43cf8260e798ae7c4
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-05-25 16:26:11 +0530

    GraphImpl1 class added

---
+++ graphs/GraphImpl1.java
@@ -0,0 +1,19 @@
+package graphs;
+
+import java.util.ArrayList;
+
+public class GraphImpl1 {
+    static class Edge {
+        int src;
+        int dst;
+
+        Edge(int s, int d) {
+            this.src = s;
+            this.dst = d;
+        }
+    }
+
+    void buildTree(ArrayList<Edge> graph[]) {
+
+    }
+}

320c45f2b678ce8b0cb6f6da0605ed1415200e87
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-05-25 16:32:37 +0530

    basic graph implemented

--- graphs/GraphImpl1.java
+++ graphs/GraphImpl1.java
@@ -13,7 +13,30 @@ public class GraphImpl1 {
         }
     }
 
-    void buildTree(ArrayList<Edge> graph[]) {
+    static void buildGraph(ArrayList<Edge> graph[]) {
+        // first we need to init all the Array at graph indexes
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
 
     }
+
+
+    public static void main(String[] args) {
+        ArrayList<Edge> graph[] = new ArrayList[4]; // vertex size is 4
+        buildGraph(graph);
+    }
+
 }

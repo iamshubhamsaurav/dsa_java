80806cf91314d8dccc970ca90e6d045c24d258d1
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-07-02 23:54:29 +0530

    Added insert method

--- tries/TriesImpl1.java
+++ tries/TriesImpl1.java
@@ -15,7 +15,27 @@ public class TriesImpl1 {
         }
     }
 
-    public static void main(String[] args) {
+    static Node root;
+
+    static void insert(String word) {
         
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a'; // get the index of char
+
+            if(root.children[idx] == null) { // if the array at that index is null then create a new Node there
+                root.children[idx] = new Node();
+            }
+            if(i == word.length() - 1) {
+                root.eow = true;
+            }
+            root = root.children[idx];
+        }
+    }
+
+    public static void main(String[] args) {
+        String words[] = {"a", "the", "their", "there", "any"};
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
     }
 }

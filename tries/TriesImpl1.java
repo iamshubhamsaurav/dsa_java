1d1f65a9d84142f08eee17c6f38a29e74070f6d3
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-07-04 23:53:54 +0530

    Added startsWith in tries

--- tries/TriesImpl1.java
+++ tries/TriesImpl1.java
@@ -56,6 +56,20 @@ public class TriesImpl1 {
 
     }
 
+    public static boolean startsWith(String prefix) {
+        Node curr = root;
+
+        for(int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+
+            if(curr.children[idx] == null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         String words[] = {"aa", "the", "their", "there", "any"};
         for (int i = 0; i < words.length; i++) {

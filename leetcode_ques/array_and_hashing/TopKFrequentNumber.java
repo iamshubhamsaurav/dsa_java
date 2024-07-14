6be90bf42b3953bfa9d7fb750dc97fa266667403
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-07-14 21:01:08 +0530

    Added PurchasingMaxItem

--- leetcode_ques/array_and_hashing/TopKFrequentNumber.java
+++ leetcode_ques/array_and_hashing/TopKFrequentNumber.java
@@ -1,8 +1,5 @@
 package leetcode_ques.array_and_hashing;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class TopKFrequentNumber {
 
     public static void main(String[] args) {
---
+++ other_questions/PurchasingMaxItems.java
@@ -0,0 +1,31 @@
+package other_questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class PurchasingMaxItems {
+
+    static int purchasingMaxItems(List<Integer> nums, int sum) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(nums);
+
+        int res = 0;
+
+        while(sum > 0 && !pq.isEmpty() && pq.peek() <= sum) {
+            sum = sum - pq.poll();
+            res++;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Integer nums[] = {1,12,5,3,11};
+        List<Integer> arr = Arrays.asList(nums);
+        int sum = 10;
+        int res = purchasingMaxItems(arr, sum);
+        System.out.println(res);
+    }
+}

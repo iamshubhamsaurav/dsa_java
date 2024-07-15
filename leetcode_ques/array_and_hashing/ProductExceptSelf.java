80fd19daaf150783641c174fc4a971d3a8709a7b
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-07-15 23:48:48 +0530

    Added ProductExceptSelf

--- leetcode_ques/array_and_hashing/ProductExceptSelf.java
+++ leetcode_ques/array_and_hashing/ProductExceptSelf.java
@@ -5,34 +5,25 @@ public class ProductExceptSelf {
         int nums[] = {1,2,3,4};
         int res[] = productExceptSelf(nums);
 
-        for(int n: res) {
-            System.out.println(n);
-        }
+        // for(int n: res) {
+        //     System.out.println(n);
+        // }
     }
 
     public static int[] productExceptSelf(int[] nums) {
         int res[] = new int[nums.length];
-        int prefix = 1;
         
         // storing prefix product
         res[0] = 1;
         for(int i = 1; i < nums.length; i++) {
-            res[i] = nums[i-1] * prefix;
-            prefix = res[i];
+            res[i] = res[i-1] * nums[i - 1];
         }
 
-        // System.out.println("Pringint prefix");
-        // for(int i = 0; i < res.length; i++) {
-        //     System.out.print(res[i] + " ");
-        // }
-
-        System.out.println();
-
-        // int postfix = 1;
-        for(int i = nums.length - 2; i >= 0; i--) {
-            res[i] = res[i] * nums[i+1];
-            nums[i] = nums[i] * nums[i+1];
-            // postfix = res[i];
+        // calc post and storing the res
+        int postfix = 1;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            res[i] = res[i] * postfix;
+            postfix = nums[i] * postfix;
         }
 
         return res;

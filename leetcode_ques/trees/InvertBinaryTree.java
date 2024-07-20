d1bae2814759aa413bb012bc0f0a63929c771358
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-07-20 20:39:01 +0530

    Added Invert Binary Tree

---
+++ leetcode_ques/trees/InvertBinaryTree.java
@@ -0,0 +1,30 @@
+package leetcode_ques.trees;
+
+public class InvertBinaryTree {
+
+
+    // Only for demo of Tree structure
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+        public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}

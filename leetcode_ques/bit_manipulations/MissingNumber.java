3cfc05cf172c4bfc61cea8e84ff057d7c6a857ee
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-08-10 23:45:11 +0530

    added xor sol for missingNumber

--- leetcode_ques/bit_manipulations/MissingNumber.java
+++ leetcode_ques/bit_manipulations/MissingNumber.java
@@ -5,6 +5,20 @@ public class MissingNumber {
     // using hashset
 
     // using xor
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xorArr = 0;
+        int xorRange = 0;
+        for(int i = 0; i < n; i++) {
+            xorArr ^= nums[i];
+        }
+
+        for(int i = 1; i <= n; i++) {
+            xorRange ^= i;
+        }
+
+        return (xorRange ^ xorArr);
+    }
 
     // using sum
     public int missingNumberUsingSum(int[] nums) {
